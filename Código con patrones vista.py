a5ea2a35d1057d1fc0c205f6b730074a8eb8a458
Ariel F.G <60789996+ariel0106fg@users.noreply.github.com>
2020-10-10 21:07:04 -0500

    Create Código con patrones vista.py

---
+++ Código con patrones vista.py
@@ -0,0 +1,9 @@
+import time
+print ("El día de hoy calculares el factorial de un número") #se indica que es lo que se va a realizar.
+time.sleep(2)
+print("Ingrese el número del cual desea obtener el factorial") # se pide el número a sacar el factorial.
+from modelo import * # nos trae el archivo que solicita y almacena los datos
+time.sleep(1)
+from controlador_factorial import *
+time.sleep(1)
+print("Fin del programa") # indica el final del programa
